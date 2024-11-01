c50201c3a0fc4265b9e84db37214265700854f77
Kássio Fernandes <kassioflima@outlook.com>
2024-11-01 10:45:16 -0300

    Add project files.

---
+++ SwaggerVersioning/Configurations/SwaggerVersioningOptions.cs
@@ -0,0 +1,32 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SwaggerVersioning.Configurations
+{
+    
+
+    public class SwaggerVersioningOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public SwaggerVersioningOptions(IApiVersionDescriptionProvider provider) => _provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = $"API {description.ApiVersion}",
+                        Version = description.ApiVersion.ToString(),
+                        Description = $"Documentação para API versão {description.ApiVersion}"
+                    });
+            }
+        }
+    }
+
+}
---
+++ SwaggerVersioning/Program.cs
@@ -0,0 +1,55 @@
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
+using SwaggerVersioning.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configurar o versionamento da API
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configuração para mostrar as versões no Swagger
+builder.Services.ConfigureOptions<SwaggerVersioningOptions>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        // Adicionar cada versão da API ao Swagger
+        var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint(
+                $"/swagger/{description.GroupName}/swagger.json",
+                description.GroupName.ToUpperInvariant());
+        }
+    });
+}
+    app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
